175f4e674e47f83a519fe5437f773044434948e2
Peter Wyrd <thewyrdguy@gmail.com>
2024-08-13 00:37:25 +0200

    Data: working (hopefully) ring buffer

--- Data.cpp
+++ Data.cpp
@@ -12,35 +12,35 @@ static uint16_t energy = 0;
 static int rssi = 0;
 static int batt = 0;
 
-#define BUFSIZE 1024
+#define BUFSIZE 384  // 2.7 seconds worth of data at 150 SPS
 static int8_t samples[BUFSIZE] = {};  // +/- 127 should be enough for display that is 240 px high
-static uint16_t rdptr = 0;
-static uint16_t wrptr = 0;
+static uint16_t rdp = 0;  // we expect 25 times more reads than writes, so use read point for the base
+static uint16_t amount = 0;
 
 void dataSend(uint16_t p_hr, uint16_t p_energy, int p_rssi, int p_num, int8_t *p_samples) {
-  int to_copy;
-  bool overrun = false;
+  int wrp, avail, rest, buf_left;
 
   std::lock_guard<std::mutex> lck(mtx);
   hr = p_hr;
   energy = p_energy;
   rssi = p_rssi;
-  if ((rdptr > wrptr && rdptr < (wrptr + p_num)) ||
-      ((rdptr + BUFSIZE) > wrptr && (rdptr + BUFSIZE) < (wrptr + p_num))) {
-      overrun = true;
-      Serial.print("Overrun: rdptr = ");
-      Serial.print(rdptr);
-      Serial.print(" wrptr = ");
-      Serial.println(wrptr);
-   }
-  do {
-    to_copy = (wrptr + p_num < BUFSIZE) ? p_num : (BUFSIZE - wrptr);
-    p_num -= to_copy;
-    std::memcpy(samples+wrptr, p_samples, to_copy * sizeof(samples[0]));
-    wrptr += to_copy;
-    if (wrptr >= BUFSIZE) wrptr = 0;
-  } while (p_num);
-  if (overrun) rdptr = wrptr;
+
+  wrp = (rdp + amount) % BUFSIZE;
+  avail = BUFSIZE - amount;
+  buf_left = BUFSIZE - wrp;
+  if (buf_left >= p_num) {
+    memcpy(samples + wrp, p_samples, p_num);
+  } else {
+    memcpy(samples + wrp, p_samples, buf_left);
+    memcpy(samples, p_samples + buf_left, p_num - buf_left);
+  }
+  if (p_num <= avail) {
+    amount += p_num;
+  } else {
+    Serial.print("Overrun by "); Serial.println(p_num - avail);
+    amount = BUFSIZE;
+    rdp = (wrp + p_num) % BUFSIZE;
+  }
 }
 
 uint16_t getHR(void) {
@@ -57,31 +57,29 @@ int getRSSI(void) {
 static int8_t buf[MAXSAMP] = {};
 
 int8_t *getSamples(int num) {
-  int i;
-  int to_copy;
+  int to_copy, to_repeat, buf_left;
 
   assert(num < MAXSAMP);
   std::lock_guard<std::mutex> lck(mtx);
-  if ((rdptr < wrptr && (rdptr + num) > wrptr) || 
-      (rdptr < (wrptr + BUFSIZE) && (rdptr + num) > (wrptr + BUFSIZE))) {
-      to_copy = (wrptr - rdptr + BUFSIZE) % BUFSIZE;
-      Serial.print("Underun: rdptr = ");
-      Serial.print(rdptr);
-      Serial.print(" wrptr = ");
-      Serial.print(wrptr);
-      Serial.print(" to_copy ");
-      Serial.println(to_copy);
-   } else {
+  if (num < amount) {
     to_copy = num;
-   }
-  for (i = 0; i < to_copy && rdptr < BUFSIZE; i++)
-    buf[i] = samples[rdptr++];
-  if (rdptr >= BUFSIZE)
-    rdptr = 0;
-  for (; i < to_copy; i++)
-    buf[i] = samples[rdptr++];
-  for (; i < num; i++)
-    buf[i] = 0;
+    to_repeat = 0;
+  } else {
+    Serial.print("Underrun, only got "); Serial.println(to_copy);
+    to_copy = amount;
+    to_repeat = num - amount;
+  }
+  buf_left = BUFSIZE - rdp;
+  if (buf_left >= to_copy) {
+    memcpy(buf, samples + rdp, to_copy);
+  } else {
+    memcpy(buf, samples + rdp, buf_left);
+    memcpy(buf + buf_left, samples, to_copy - buf_left);
+  }
+  if (to_repeat)
+    memset(buf + to_copy, samples[(rdp + to_copy - 1) % BUFSIZE], to_repeat);
+
+  rdp = (rdp + to_copy) % BUFSIZE;
+  amount -= to_copy;
   return buf;
-  // TODO handle buffer underrun
 }
