9a89fa9d9f906ecff98dd88e840a4613dcb2ee4b
Peter Wyrd <thewyrdguy@gmail.com>
2024-08-24 00:13:18 +0200

    Borrow crc8 library from Jim Studt

---
+++ crc8.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+#include "crc8.h"
+
+// https://github.com/PaulStoffregen/OneWire/blob/master/OneWire.cpp
+
+// The 1-Wire CRC scheme is described in Maxim Application Note 27:
+// "Understanding and Using Cyclic Redundancy Checks with Maxim iButton Products"
+//
+// Dow-CRC using polynomial X^8 + X^5 + X^4 + X^0
+// Tiny 2x16 entry CRC table created by Arjen Lentz
+// See http://lentz.com.au/blog/calculating-crc-with-a-tiny-32-entry-lookup-table
+
+static const uint8_t PROGMEM dscrc2x16_table[] = {
+	0x00, 0x5E, 0xBC, 0xE2, 0x61, 0x3F, 0xDD, 0x83,
+	0xC2, 0x9C, 0x7E, 0x20, 0xA3, 0xFD, 0x1F, 0x41,
+	0x00, 0x9D, 0x23, 0xBE, 0x46, 0xDB, 0x65, 0xF8,
+	0x8C, 0x11, 0xAF, 0x32, 0xCA, 0x57, 0xE9, 0x74
+};
+
+uint8_t crc8(const uint8_t *addr, uint8_t len) {
+	uint8_t crc = 0;
+
+	while (len--) {
+		crc = *addr++ ^ crc;  // just re-using crc as intermediate
+		crc = pgm_read_byte(dscrc2x16_table + (crc & 0x0f)) ^
+		pgm_read_byte(dscrc2x16_table + 16 + ((crc >> 4) & 0x0f));
+	}
+
+	return crc;
+}
---
+++ crc8.h
@@ -0,0 +1,3 @@
+#pragma once
+
+uint8_t crc8(const uint8_t *addr, uint8_t len);
